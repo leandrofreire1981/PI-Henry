de1bf5079ee6747dea74cc7a6906ae95da3c0ea1
Leandro <freire.leandro@hotmail.com>
2022-09-26 19:42:16 -0300

    commit

--- api/src/app.js
+++ api/src/app.js
@@ -23,7 +23,7 @@ server.use((req, res, next) => {
 });
 
 server.use('/', routes);
-
+console.log('hola')
 // Error catching endware.
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   const status = err.status || 500;
--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,33 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id: {
+      type: DataTypes.UUID, //numero aleatorio de letras y numeros unico
+      defaultValue: DataType.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    summary: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    healthScore: {
+      type: DataTypes.INTEGER
+    },
+    instructions: { // instrucciones paso a paso
+      type: DataTypes.ARRAY(DataTypes.JSONB)
+    },
+    img: {
+      type: DataTypes.STRING,
+      defaultValue: 'https://blogdigital.es/wp-content/uploads/2015/09/imagen-no-encontrada.jpg'
+    }
+  },
+    {
+      timestamps: false
+    }
+  );
 };
